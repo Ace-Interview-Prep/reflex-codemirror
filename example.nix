e657995577441b796fc52094086ce92ccf530703
Kyle Jensen <kyle.jensen72@gmail.com>
2025-06-18 23:08:23 -0400

    Update example.nix

--- example.nix
+++ example.nix
@@ -7,22 +7,15 @@ let
     rev = "d25152d30a76001d8a22ae96ee55f500180dd063";
   };
 
-  reflexUtilsSrc = fetchGit {
-    url = https://github.com/atidot/reflex-utils;
-    rev = "9ffda7ef04c3d538fdf2ae8d6f3eddcd456de8ab";
-  };
-
   reflex-platform = import reflexPlatformSrc {};
 in
 reflex-platform.project({ pkgs, ... }: {
   packages = {
     reflex-codemirror = ../reflex-codemirror;
-    reflex-utils      = reflexUtilsSrc;
   };
 
   shells = {
     ghcjs = [ "reflex-codemirror"
-              "reflex-utils"
             ];
   };
 })
